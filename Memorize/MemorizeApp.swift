66614f5a9b6dbe6a7687964aa7b597e848424b5d
Jorgemz🤨 <jorge_mz@icloud.com>
2023-09-25 10:33:21 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by JG on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
