3c7006d91465b6fa76db99eae8d0f19a9bb75c35
Jorgemz🤨 <jorge_mz@icloud.com>
2023-09-26 17:55:19 -0500

    RT 5

--- Memorize/MemorizeGame.swift
+++ Memorize/MemorizeGame.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
 
    private(set) var cards: Array<Card>
 
@@ -16,24 +16,55 @@ struct MemoryGame<CardContent> {
       // add numberOfPairsOfCards x 2 cards
       for pairIndex in 0..<max(2, numberOfPairsOfCards) {
          let content = cardContentFactory(pairIndex)
-         cards.append(Card(content: content))
-         cards.append(Card(content: content))
+         cards.append(Card(content: content, id: "\(pairIndex+1)a"))
+         cards.append(Card(content: content, id: "\(pairIndex+1)b"))
       }
    }
 
-   func choose(card: Card) {
+   private var indexOfTheOneOnlyFaceCard: Int? {
+      get { cards.indices.filter { index in cards[index].isFaceUp }.only }
+      set { cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) } }
+   }
 
+   mutating func choose(_ card: Card) {
+      if let chooseIndex = cards.firstIndex(where: { $0.id == card.id }) {
+         if !cards[chooseIndex].isFaceUp && !cards[chooseIndex].isMatched {
+
+            if let potencialMatchIndex = indexOfTheOneOnlyFaceCard {
+               if cards[chooseIndex].content == cards[potencialMatchIndex].content {
+                  cards[chooseIndex].isMatched = true
+                  cards[potencialMatchIndex].isMatched = true
+               }
+            } else {
+               indexOfTheOneOnlyFaceCard = chooseIndex
+            }
+
+            cards[chooseIndex].isFaceUp = true
+         }
+      }
    }
 
    mutating func shuffle() {
       cards.shuffle()
+      print(cards)
    }
 
-   struct Card {
+   struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
 
-      var isFaceUp = true
+      var isFaceUp = false
       var isMatched = false
       let content: CardContent
 
+      let id: String
+
+      var debugDescription: String {
+         "\(id): \(content) \(isFaceUp ? "up" : "down")\(isMatched ? "matched" : "")"
+      }
+   }
+}
+
+extension Array {
+   var only: Element? {
+      return count == 1 ? first : nil
    }
 }
