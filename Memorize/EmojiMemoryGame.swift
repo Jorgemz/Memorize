ece60701d16e47f60eb9d25ae9fa4bd19b8e741b
Jorgemz🤨 <jorge_mz@icloud.com>
2023-09-26 11:24:50 -0500

    RT 4

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,36 @@
 //  Created by JG on 25/09/23.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+
+   private static let emojis = ["👻", "🎃", "🕷️", "👿", "💀", "❄️", "🧙🏻", "🙀", "👹", "😱", "☠️", "🍭"]
+   private static func createMemoryGame() -> MemoryGame<String> {
+      MemoryGame<String>( numberOfPairsOfCards: 16) { pairIndex in
+         if emojis.indices.contains(pairIndex) {
+            emojis[pairIndex]
+         } else {
+            "⁉️"
+         }
+      }
+   }
+
+   @Published
+   private var model = createMemoryGame()
+
+   var cards: Array<MemoryGame<String>.Card> {
+      model.cards
+   }
+
+   // MARK: - Intents
+
+   func shuffle() {
+      model.shuffle()
+   }
+
+   func choose(_ card: MemoryGame<String>.Card) {
+      model.choose(card: card)
+   }
+
+}
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -9,22 +9,27 @@ import SwiftUI
 
 struct EmojiMemoryGameView: View {
 
+   @ObservedObject
    var viewModel: EmojiMemoryGame
 
-   let emojis = ["👻", "🎃", "🕷️", "👿", "💀", "❄️", "🧙🏻", "🙀", "👹", "😱", "☠️", "🍭"]
-
    var body: some View {
-      ScrollView {
-         cards
+      VStack {
+         ScrollView {
+            cards
+         }
+         Button("Shuffle") {
+            viewModel.shuffle()
+         }
       }
       .padding()
   }
 
    var cards: some View {
-      LazyVGrid(columns: [GridItem(.adaptive(minimum: 120 ))]) {
-         ForEach(emojis.indices, id: \.self) { index in
-            CardView(content: emojis[index])
+      LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
+         ForEach(viewModel.cards.indices, id: \.self) { index in
+            CardView(viewModel.cards[index])
                .aspectRatio(2/3, contentMode: .fit)
+               .padding(4)
          }
       }
       .foregroundStyle(Color.orange)
@@ -34,8 +39,11 @@ struct EmojiMemoryGameView: View {
 
 struct CardView: View {
 
-   let content: String
-   @State var isFaceUp = true
+   let card: MemoryGame<String>.Card
+
+   init(_ card: MemoryGame<String>.Card) {
+      self.card = card
+   }
 
    var body: some View {
       ZStack {
@@ -43,19 +51,19 @@ struct CardView: View {
          Group {
             base.fill(.white)
             base.strokeBorder(lineWidth: 2)
-            Text(content).font(.largeTitle)
+            Text(card.content)
+               .font(.system(size: 200))
+               .minimumScaleFactor(0.01)
+               .aspectRatio(1, contentMode: .fit)
          }
-         .opacity(isFaceUp ? 1 : 0)
-         base.fill().opacity(isFaceUp ? 0 : 1)
-      }
-      .onTapGesture {
-         isFaceUp.toggle()
+         .opacity(card.isFaceUp ? 1 : 0)
+         base.fill().opacity(card.isFaceUp ? 0 : 1)
       }
    }
 }
 
 #Preview {
-   EmojiMemoryGameView(viewModel: <#EmojiMemoryGame#>)
+   EmojiMemoryGameView(viewModel: EmojiMemoryGame())
 }
 
 import os.log
